359f47df8f4a6835a079f1aa668cd331f6443ad3
Laastar <machicaor@gmail.com>
2018-02-24 21:26:37 -0600

    Add WildForceMegazord_SwordandShieldMode.sh

--- WildForceMegazord_SwordandShieldMode.sh
+++ WildForceMegazord_SwordandShieldMode.sh
@@ -40,6 +40,6 @@ fi
 sleep 5
 clear
 fortune | cowsay -f dragon | lolcat
-PS1='\e[0;36m\H.\u\e[m \e[0;31m@\e[m \e[0;34m\W\e[m \e[0;31m@\e[m \e[0;35m\T:\e[m\n\e[0;36mInsert commands:\e[m'
+PS1='\e[0;36m\H.\u\e[m \e[0;31m@\e[m \e[0;34m\W\e[m \e[0;31m@\e[m \e[0;35m\T\e[m\n\e[0;36mInsert commands:\e[m'
 #gnome-terminal & disown
 #sh /home/laastar/PowerRangersFuerzaSalvaje/BlueSharkZord.sh
