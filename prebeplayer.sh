8fe1b7e09c362d6cf4e0b076cd917c20f1081fc6
bnl75 <36786896+bnl75@users.noreply.github.com>
2018-03-06 05:06:03 -0600

    Add files via upload
    Interface

---
+++ prebeplayer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+
+echo -e "\e[96;107;1;7m  ___________________________________________________________________________________________________________  \e[0m"
+echo -e "\e[96;107;1;7m |                                                                                                           | \e[0m"
+echo -e "\e[96;107;1;7m |                                                                                                           | \e[0m"
+echo -e "\e[96;107;1;7m |                                     BIENVENIDO A LA PREBEPLAYER                                           | \e[0m"
+echo -e "\e[96;107;1;7m |                                                                                                           | \e[0m"
+echo -e "\e[96;107;1;7m |                                                                                                           | \e[0m"
+echo -e "\e[96;107m |                                                                                                           | \e[0m"
+echo -e "\e[96;107m |	        Este programa te permitirá escuchar tus canciones desde la prebeshell                        | \e[0m"
+echo -e "\e[96;107m |	        A continuación te mostramos una lista de opciones para manejar la prebeplayer...             | \e[0m"
+echo -e "\e[96;107m |                          Por favor ingresa tu contraseña para poder continuar                             | \e[0m"
+echo -e "\e[96;107m |___________________________________________________________________________________________________________| \e[0m"
+echo -e "\e[96;107m                                                                                                               \e[0m"
+
+read -p "Presiona [Enter]..."
+sudo apt-get install mpg123
+clear
+
