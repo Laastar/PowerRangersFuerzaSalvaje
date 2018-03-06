36bfdf089c83c5ae8822651c5b94ee08051498c7
bnl75 <36786896+bnl75@users.noreply.github.com>
2018-03-06 07:19:53 -0600

    Creación de menú para el usuario
    Menú para seleccionar una forma de reproducción o salir de la prebeplayer

--- prebeplayer.sh
+++ prebeplayer.sh
@@ -19,3 +19,60 @@ read -p "Presiona [Enter]..."
 sudo apt-get install mpg123
 clear
 
+function menu
+{
+echo -e "		   M E N Ú   P R I N C I P A L		\n"
+echo "Seleccione una opción"
+echo "1.- Reproducir una sola canción"
+echo "2.- Reproducir todas las canciones de un directorio (en orden)"
+echo "3.- Reproducir todas las canciones de un directorio aleatoriamente (de forma finita)"
+echo "4.- Reproducir todas las canciones de un directorio aleatoriamente (de forma infinita)"
+echo "5.- Salir"
+echo "Opción [1-5]: "
+}
+
+menu
+
+function opciones
+{
+read opcion
+case $opcion in
+	1)
+		echo "Ingresa la ruta y el nombre de la canción de la siguiente manera: /ruta/\"canción.mp3\" y posteriormente presiona la letra \"h\" para ver las teclas de control";
+		read cancion; mpg123 --title -C $cancion;
+		clear;
+		echo -e "--- FIN DE LA REPRODUCCIÓN --- \nPresione [Enter] para regresar al menú principal..."; read -p ""; clear; 
+		menu; opciones;;
+
+	2)
+		echo "Ingresa la ruta del directorio de la siguiente manera: /ruta/directorio_con_las_canciones y posteriormente presiona la letra \"h\" para ver las teclas de control";
+                read carpeta; mpg123 --title -C $carpeta/*;
+		clear;
+		echo -e "--- FIN DE LA REPRODUCCIÓN --- \nPresione [Enter] para regresar al menú principal..."; read -p ""; clear; 
+                menu; opciones;;
+
+
+	3)
+		echo "Ingresa la ruta del directorio de la siguiente manera: /ruta/directorio_con_las_canciones y posteriormente presiona la letra \"h\" para ver las teclas de control";
+                read carpeta; mpg123 --title --shuffle -C $carpeta/*;
+                clear;
+                echo -e "--- FIN DE LA REPRODUCCIÓN --- \nPresione [Enter] para regresar al menú principal..."; read -p ""; clear; 
+                menu; opciones;;
+
+	4)
+		echo "Ingresa la ruta del directorio de la siguiente manera: /ruta/directorio_con_las_canciones y posteriormente presiona la letra \"h\" para ver las teclas de control";
+                read carpeta; mpg123 --title --random -C $carpeta/*;
+                clear;
+                echo -e "--- FIN DE LA REPRODUCCIÓN --- \nPresione [Enter] para regresar al menú principal..."; read -p ""; clear; 
+                menu; opciones;;
+
+
+
+	5)	read -p "Presiona [Enter] para salir"; clear; exit;;
+
+esac
+}
+
+
+
+opciones
