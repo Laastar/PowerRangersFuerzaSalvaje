ce44f3ffff14310675a696b807d0ad718e613658
Laastar <machicaor@gmail.com>
2018-03-08 23:11:40 -0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -41,7 +41,7 @@ else
 fi
 sleep 5
 clear
-echo "Bienvenido a la Bahia de las Marianas" | fortune | cowsay -f dragon | lolcat
+echo "Bienvenido a la Bahia de las Marianas" | cowsay -f dragon | lolcat
 PS1='\e[0;36m\H.\u\e[m \e[0;31m@\e[m \e[0;34m\W\e[m \e[0;31m@\e[m \e[0;35m\T\e[m\n\e[0;36mInsert commands:\e[m'
 #gnome-terminal & disown
 #sh /home/laastar/PowerRangersFuerzaSalvaje/BlueSharkZord.sh
