571af0ecfc2d8ca65b32af69d01c81682941c2e8
Laastar <machicaor@gmail.com>
2018-03-08 23:08:47 -0600

    Add juego azar

--- Cagliostro.sh
+++ Cagliostro.sh
@@ -2,14 +2,14 @@
 
 if(( "$BahiadeLasMarianas" == 1))
 then
-	echo"Tienes 2 monedas"
-	echo"Si presionas el boton hay 50% de probabilidad de duplicar"
-	echo"tus monedas y otro 50% de perder todo"
-	echo"¿Apuestas? (y/n)"
+	echo "Tienes 2 monedas"
+	echo "Si presionas el boton hay 50% de probabilidad de duplicar"
+	echo "tus monedas y otro 50% de perder todo"
+	echo "¿Apuestas? (y/n)"
 	read response
-	if (( "$Response" == "y"))
-		then
-			awk -v min=1 -v max=2 'BEGIN{srand(); print int(min+rand()*max-min+1))}'
+	if [ "$Response" == "y" ]
+	then
+			awk -v min=1 -v max=2 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
 	else
 		echo " :/  Coward XD"
 	fi
