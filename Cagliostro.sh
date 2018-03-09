b75c004c76680d2b68c16f2730798d3c0958e6c8
Laastar <machicaor@gmail.com>
2018-03-09 00:54:07 -0600

    Create Cagliostro.sh

--- Cagliostro.sh
+++ Cagliostro.sh
@@ -3,26 +3,30 @@
 if(( "$BahiadeLasMarianas" == 1))
 then
 	clear
-	echo "Tienes 2 monedas"
+	YELLOW='\033[1;33m'
+	echo -e "${YELLOW}Tienes 2 monedas"
 	monedas=2
 	DIFF=$((2))
 	RANDOM=$$
-	echo "Si presionas el boton hay 50% de probabilidad de duplicar"
-	echo "tus monedas y otro 50% de perder todo"
-	echo "Puedes hacerlo un maximo de 5 veces"
-	echo "¿Apuestas? (y/whatever key you want to press)"
+	echo "		Si presionas el boton hay 50% de probabilidad de duplicar"
+	echo "		         tus monedas y otro 50% de perder todo"
+	echo "			     Se tira cinco veces la moneda"
+	echo "¿Apuestas? (y/whatever key you want to press)" | pokemonsay -p Blastoise
 	read response
 	if(("$response" == "y"))
 	then
-		valor=$(($RANDOM%$DIFF))
-		if(("$valor" == 1 && "$monedas" != 0))
-		then
-			monedas=monedas * 2
-			echo "Tus monedas ahora son $monedas"
-		else
-			echo "Te quedaste sin monedas :C"
-			break
-		fi
+		for i in `seq 5`
+		do
+			valor=$(($RANDOM%$DIFF))
+			if(("$valor" == 1 && "$monedas" != 0))
+			then
+				monedas=$(($monedas * 2))
+				echo "Tus monedas ahora son $monedas"
+			else
+				echo "Te quedaste sin monedas :C" | pokemonsay -p Squirtle
+				break
+			fi
+		done
 	else
 		echo " :/  Coward XD"
 	fi
