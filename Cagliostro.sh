1eea19f0893f5394d7cdea56f6d9fc90a9ae551d
Laastar <machicaor@gmail.com>
2018-03-08 23:12:52 -0600

    Update Cagliostro.sh

--- Cagliostro.sh
+++ Cagliostro.sh
@@ -7,9 +7,9 @@ then
 	echo"tus monedas y otro 50% de perder todo"
 	echo"¿Apuestas? (y/n)"
 	read response
-	if (( "$Response" == "y"))
+	if  [ "$Response" == "y" ]
 		then
-			awk -v min=1 -v max=2 'BEGIN{srand(); print int(min+rand()*max-min+1))}'
+			awk -v min=1 -v max=2 'BEGIN{srand(); print int(min+rand()*(max-min+1))}'
 	else
 		echo " :/  Coward XD"
 	fi
